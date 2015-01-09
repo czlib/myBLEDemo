1584271124cf06b70c5671ebb099a626d5362dfd
chenzl <zhili.chen@gajah.com.hk>
2015-01-09 14:04:24 -0800

    first commit

---
+++ myBLEDemo/myBLEDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myBLEDemo
+//
+//  Created by zl.c on 15-1-9.
+//  Copyright (c) 2015年 czl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myBLEDemo/myBLEDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myBLEDemo
+//
+//  Created by zl.c on 15-1-9.
+//  Copyright (c) 2015年 czl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
